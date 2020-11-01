ccdb5776ed8bbe23def13615387efdda3ee58a33
kartik malik <kartikforhack@gmail.com>
2020-11-01 19:20:45 +0530

    auth

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,13 +1,14 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const auth = require('../middleware/auth')
-const userController = require("../controllers/users");
+const auth = require('../middleware/auth');
+const userController = require('../controllers/users');
 
-router.post("/signup", userController.create_a_user);
-router.post("/login", userController.login);
-router.delete("/:userid", userController.delete_a_user);
-router.get("/getUsers", userController.getUsers);
-router.get("/getMyTeams", userController.getMyTeams);
-router.get("/logout",auth,userController.logout)
+router.post('/signup', userController.create_a_user);
+router.post('/login', userController.login);
+router.post('/validate', userController.validate);
+router.delete('/:userid', userController.delete_a_user);
+router.get('/getUsers', userController.getUsers);
+router.get('/getMyTeams', userController.getMyTeams);
+router.get('/logout', auth, userController.logout);
 
 module.exports = router;
