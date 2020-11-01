efda7e00c65673adb67650e3bf8dbaf87e676e74
Prerna Budhraja <prernabudhraja8@gmail.com>
2020-11-01 19:25:26 +0530

    auth

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,2 +1,2 @@
 exports.MONGO_URI =
-  "mongodb+srv://malik:malik@businessideas-dhipt.mongodb.net/cash-flow?retryWrites=true&w=majority";
+  'mongodb+srv://malik:malik@businessideas-dhipt.mongodb.net/cashflow?retryWrites=true&w=majority';
