59820015a3fab8068b4a0c5bc81ee54d8dcaff21
kartik malik <kartikforhack@gmail.com>
2020-11-07 01:31:34 +0530

    asdf

--- src/controllers/transactions.js
+++ src/controllers/transactions.js
@@ -2,32 +2,41 @@ const mongoose = require('mongoose');
 const Team = require('../models/team');
 const User = require('../models/user');
 const Place = require('../models/place');
+const Transaction = require('../models/transaction');
 const {findOne} = require('../models/team');
 
 exports.addTransactions = async (req, res, next) => {
   try {
+    let transactions = [];
+    let transactionIds = [];
+    req.body.transactions.forEach((transaction) => {
+      const tempTrans = new Transaction({
+        _id: mongoose.Types.ObjectId(),
+        from: transaction.from,
+        to: transaction.to,
+        amount: transaction.amount,
+        settled: false,
+      });
+      transactionIds.push(tempTrans._id);
+      transactions.push(tempTrans);
+    });
+
     let place = new Place({
       _id: mongoose.Types.ObjectId(),
       bill: req.body.bill,
       name: req.body.placeName,
-      transactions: [],
+      transactions: transactionIds,
     });
 
-    const teamId = req.body.id;
-    const from = req.body.from.split(' ');
-    const to = req.body.to.split(' ');
-    const amount = req.body.amount.split(' ');
-
-    const team = await Team.findOne({teamId: teamId});
-    team.transactions.from = team.transactions.from.concat(from);
-    team.transactions.to = team.transactions.to.concat(to);
-    team.transactions.amount = team.transactions.amount.concat(amount);
-
-    await Team.findOneAndUpdate({teamId: teamId}, team);
-    res.json({message: 'SUCCESS!'});
+    await Team.findByIdAndUpdate(req.body.teamId, {$push: {places: place._id}});
+    await place.save();
+    await Transaction.insertMany(transactions);
+    return res.send({place, transactions: transactions});
   } catch (e) {
-    console.log(e);
-    res.json({message: 'ERROR!'});
+    if (e) {
+      console.log(e);
+      return res.json({message: 'ERROR!'});
+    }
   }
 };
 
