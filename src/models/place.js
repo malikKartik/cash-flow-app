8c3300a75831187ce62cc55f19e05564ee94f8e9
kartik malik <kartikforhack@gmail.com>
2020-11-07 17:58:26 +0530

    get transactions resp

--- src/models/place.js
+++ src/models/place.js
@@ -4,7 +4,7 @@ const placeSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   bill: String,
   name: {type: String, required: true},
-  transactions: [{type: mongoose.Schema.Types.ObjectId, ref: 'Transactions'}],
+  transactions: [{type: mongoose.Schema.Types.ObjectId, ref: 'Transaction'}],
 });
 
 module.exports = mongoose.model('Place', placeSchema);
